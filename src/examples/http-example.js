eb995136f74b2bc57d6222dbd118eb83f2b57b0c
Orel Biton <orelbiton27@gmail.com>
2020-07-15 23:06:47 +0300

    replaced explicit function with console.log

--- src/examples/http-example.js
+++ src/examples/http-example.js
@@ -1,6 +1,4 @@
 const { fromHttpRequest } = require('../utils/http');
 
 fromHttpRequest('https://orels-moviedb.herokuapp.com/directors')
-    .subscribe(directors => {
-        console.log(directors);
-    });
+    .subscribe(console.log);
--- src/exercises/exercise1.js
+++ src/exercises/exercise1.js
@@ -1,2 +1,7 @@
-// your code goes here...
-// good luck
+const {fromHttpRequest} = require('../utils/http');
+
+fromHttpRequest('https://orels-moviedb.herokuapp.com/directors')
+    .pipe(
+        // add the proper operators to emit the correct information
+    )
+    .subscribe(console.log);
