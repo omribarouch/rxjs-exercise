e2cab81a369063e3cd0c097801cc64e2223fb825
Orel Biton <orelbiton27@gmail.com>
2020-07-20 11:42:35 +0300

    added complex example

---
+++ src/examples/complex-example.js
@@ -0,0 +1,46 @@
+const {fromHttpRequest} = require('../utils/http');
+const {from} = require('rxjs');
+const {mergeAll, mergeMap, map, max, toArray, filter, groupBy, count} = require('rxjs/operators');
+
+// Emit the year in which there were most thriller movies
+
+// I created this function because making a request for the server is very slow.
+function getGenre(id) {
+    const genres = [{"id": 1, "name": "thriller"}, {"id": 2, "name": "mystery"}, {"id": 3, "name": "western"}, {
+        "id": 4,
+        "name": "drama"
+    }, {"id": 5, "name": "music"}, {"id": 6, "name": "fantasy"}, {"id": 7, "name": "war"}, {
+        "id": 8,
+        "name": "romance"
+    }, {"id": 9, "name": "crime"}, {"id": 10, "name": "sport"}, {"id": 11, "name": "comedy"}, {
+        "id": 12,
+        "name": "biography"
+    }, {"id": 13, "name": "history"}, {"id": 14, "name": "horror"}, {"id": 15, "name": "adventure"}, {
+        "id": 16,
+        "name": "musical"
+    }, {"id": 17, "name": "action"}, {"id": 18, "name": "sci_fi"}, {"id": 19, "name": "film_noir"}, {
+        "id": 20,
+        "name": "animation"
+    }, {"id": 21, "name": "family"}];
+    return genres.find(genre => genre.id === id);
+}
+
+fromHttpRequest('https://orels-moviedb.herokuapp.com/movies')
+    .pipe(
+        mergeAll(),
+        mergeMap(movie => from(movie.genres).pipe(
+            map(genreId => getGenre(genreId).name),
+            toArray(),
+            map(genres => {
+                return {
+                    year: parseInt(movie.year),
+                    genres: genres
+                }
+            })
+        )),
+        filter(movie => movie.genres.includes('thriller')),
+        groupBy(movie => movie.year),
+        mergeMap(group => group.pipe(count(), map(movieCount => [group.key, movieCount]))),
+        max((a, b) => a[1] < b[1] ? -1 : 1)
+    ).subscribe(console.log);
+
